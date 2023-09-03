395b56ff82ea2b4739fec321fe1d23e8b7e88a6e
Sabarish-2 <sabarishh03@gmail.com>
2023-09-03 20:16:47 +0530

    Color change Check!!

---
+++ Game/Assets/colorChange.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+}
